775c412d8374c2b0e9ebdb4efc5b5adce7d57b23
Shawn Jiang <shawn@salesfloor.net>
2026-04-24 19:23:00 -0400

    feat: add list command to show tracked commands

---
+++ cmd/list.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/shengyongjiang/ocheetsheet/internal/config"
+	"github.com/shengyongjiang/ocheetsheet/internal/store"
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all commands with tracked state",
+	Args:  cobra.NoArgs,
+	RunE:  runList,
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
+
+func runList(cmd *cobra.Command, args []string) error {
+	cfgPath := flagConfigPath
+	if cfgPath == "" {
+		cfgPath = config.DefaultConfigPath()
+	}
+	cfg, err := config.Load(cfgPath)
+	if err != nil {
+		return fmt.Errorf("load config: %w", err)
+	}
+
+	st, err := store.NewJSONStore(cfg.StateFile)
+	if err != nil {
+		return fmt.Errorf("load state: %w", err)
+	}
+
+	pages := st.ListTrackedPages()
+	if len(pages) == 0 {
+		fmt.Println("No tracked commands yet. Use `ocs <command> -i` to start learning.")
+		return nil
+	}
+
+	fmt.Println("Tracked commands:")
+	for _, p := range pages {
+		fmt.Printf("  %s\n", p)
+	}
+	return nil
+}
